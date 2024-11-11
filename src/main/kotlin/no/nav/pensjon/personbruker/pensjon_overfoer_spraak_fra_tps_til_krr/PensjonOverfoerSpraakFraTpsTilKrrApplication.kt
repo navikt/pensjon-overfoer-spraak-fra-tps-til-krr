31155dae472cbca1204c1383ab6ab64137c2efe1
Per Christian Moen <per.christian.moen@nav.no>
2024-11-11 10:38:08 +0100

    Laget av Spring initializr
    Co-authored-by: Gard Sæbø <gard.sabo@nav.no>

---
+++ src/main/kotlin/no/nav/pensjon/personbruker/pensjon_overfoer_spraak_fra_tps_til_krr/PensjonOverfoerSpraakFraTpsTilKrrApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.personbruker.pensjon_overfoer_spraak_fra_tps_til_krr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PensjonOverfoerSpraakFraTpsTilKrrApplication
+
+fun main(args: Array<String>) {
+	runApplication<PensjonOverfoerSpraakFraTpsTilKrrApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/pensjon/personbruker/pensjon_overfoer_spraak_fra_tps_til_krr/PensjonOverfoerSpraakFraTpsTilKrrApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pensjon.personbruker.pensjon_overfoer_spraak_fra_tps_til_krr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PensjonOverfoerSpraakFraTpsTilKrrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
